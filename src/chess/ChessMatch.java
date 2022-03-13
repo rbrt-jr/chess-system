6e19897a942b1b6901f436c3238f6d2e7b2636d0
Roberto Silva <roberto_silva_jr@outlook.pt>
2022-03-13 15:34:48 +0000

    a better handling for promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,5 @@
 package chess;
 
-import java.nio.channels.IllegalSelectorException;
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -175,31 +173,30 @@ public class ChessMatch {
 			throw new IllegalStateException("There is no piece to be promoted.");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion.");
+			return promoted;
 		}
 
 		Position pos = promoted.getChessPosition().toPosition();
 		Piece p = board.removePiece(pos);
 		piecesOnTheBoard.remove(p);
-		
+
 		ChessPiece newPiece = newPiece(type, promoted.getColor());
 		board.placePiece(newPiece, pos);
 		piecesOnTheBoard.add(newPiece);
-		
+
 		return newPiece;
 
 	}
-	
+
 	private ChessPiece newPiece(String type, Color color) {
-		if(type.equals("B")) return  new Bishop(board, color);
-		if(type.equals("N")) return  new Knight(board, color);
-		if(type.equals("Q")) return  new Queen(board, color);
-		return  new Rook(board, color);
+		if (type.equals("B"))
+			return new Bishop(board, color);
+		if (type.equals("N"))
+			return new Knight(board, color);
+		if (type.equals("Q"))
+			return new Queen(board, color);
+		return new Rook(board, color);
 	}
-	
-	
-	
-	
 
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
 		ChessPiece aux = (ChessPiece) board.removePiece(target);
